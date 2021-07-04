187656dcc0c96dec7d79dc359f34921021a059aa
Hyukjin Kwon <hjnim0909@gmail.com>
2021-07-04 09:20:32 +0900

    Sound Added

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CARROT_SIZE = 80;
-const CARROT_COUNT = 6;
+const CARROT_COUNT = 8;
 const BUG_COUNT = 6;
 const GAME_DURATION = 5;
 
@@ -14,6 +14,12 @@ const gameScore = document.querySelector(".game__score");
 const popUp = document.querySelector(".pop-up");
 const popUpText = document.querySelector(".pop-up__message");
 const popUpRefresh = document.querySelector(".pop-up__refresh");
+
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const alertSound = new Audio("./sound/alert.wav");
+const bgSound = new Audio("./sound/bg.mp3");
+const bugSound = new Audio("./sound/bug_pull.mp3");
+const winSound = new Audio("./sound/game_win.mp3");
 
 let started = false;
 let score = 0;
@@ -40,6 +46,7 @@ function startGame() {
   showStopButton();
   showTimerAndScore();
   startGameTimer();
+  playSound(bgSound);
 }
 
 function stopGame() {
@@ -47,11 +54,20 @@ function stopGame() {
   stopGameTimer();
   hideGameButton();
   showPopUpWithText("REPLAY?");
+  playSound(alertSound);
+  stopSound(bgSound);
 }
 
 function finishGame(win) {
   started = false;
   hideGameButton();
+  if (win) {
+    playSound(winSound);
+  } else {
+    playSound(bugSound);
+  }
+  stopGameTimer();
+  stopSound(bgSound);
   showPopUpWithText(win ? "YOU WON!" : "YOU LOST");
 }
 
@@ -82,6 +98,7 @@ function showStopButton() {
   const icon = gameBtn.querySelector(".fas");
   icon.classList.add("fa-stop");
   icon.classList.remove("fa-play");
+  gameBtn.style.visibility = "visible";
 }
 
 function hideGameButton() {
@@ -103,6 +120,7 @@ function hidePopUp() {
 }
 
 function initGame() {
+  score = 0;
   field.innerHTML = "";
   gameScore.innerHTML = CARROT_COUNT;
   // 벌레와 당근을 생성한뒤 field에 추가해준다.
@@ -119,15 +137,24 @@ function onFieldClick(event) {
     //당근!!
     target.remove();
     score++;
+    playSound(carrotSound);
     updateScoreBoard();
     if (score === CARROT_COUNT) {
       finishGame(true);
     }
   } else if (target.matches(".bug")) {
     //벌레
-    stopGameTimer();
     finishGame(false);
   }
+}
+
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function stopSound(sound) {
+  sound.pause();
 }
 
 function updateScoreBoard() {
